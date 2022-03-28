57639ca72480d7d576442064a9098dcaa7fe588c
SamuraySC <samurayiskl@gmail.com>
2022-03-28 18:48:23 +0300

    Update latestReviewGrid.js
    added linking to review page from reviews on main

--- src/latestReviewGrid.js
+++ src/latestReviewGrid.js
@@ -2,6 +2,7 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import {useNavigate} from 'react-router-dom';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.action.hover,
@@ -12,10 +13,11 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 const LatestReviewGrid = ({latestReviews}) => {
+    const navigate = useNavigate();
     return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {latestReviews.map((review) => (
-            <Grid item xs={12} sm={6} md={4} key={review.id}>
+            <Grid item xs={12} sm={6} md={4} key={review.id} onClick={() => {navigate(`/review/${review.id}`)}} >
              <Item  variant="outlined">
              <Typography gutterBottom variant="h3" component="div">
                  {review.name}
