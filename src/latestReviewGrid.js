86ba450e7c04360299fee8a6210547d01a927356
SamuraySC <samurayiskl@gmail.com>
2022-04-05 18:55:59 +0300

    Update latestReviewGrid.js

--- src/latestReviewGrid.js
+++ src/latestReviewGrid.js
@@ -16,7 +16,6 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const LatestReviewGrid = ({latestReviews}) => {
     const navigate = useNavigate();
-    console.log(latestReviews);
     return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {latestReviews.map((review) => (
